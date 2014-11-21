c8b80f2d1dc6ffa55bc1065ff4ef475c1890bbcf
toshle05 <todorjelev36@gmail.com>
2014-11-21 21:34:19 +0200

    Update 7.2DArraysTasks.cpp
    trololo

--- Exercise_17.11/7.2DArraysTasks.cpp
+++ Exercise_17.11/7.2DArraysTasks.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
 	int arr[4][4] = { {1,7,8,-5},
-					  {6,5,4,7},
-					  {2,15,6,12},
-					  {-100,12,700,6}
-					};
+			  {6,5,4,7},
+			  {2,15,6,12},
+			  {-100,12,700,6}
+			};
 
 	// 1. At the main diagonal
 	//	when we want to do something at the main diagonal this happens when i == j
@@ -21,12 +21,12 @@ int main()
 
 	//The clearly see this write the matrix on paper and the indices for rows and cols:
 	/*							
-										 0   1   2  3   - indices for the column (j)
-			index for first row		0	 1   7   8  -5
-			index for second row	1	 6   5   4  7
-			index for third row		2	 2   15  6  12
+						      0   1   2  3   - indices for the column (j)
+			index for first row	0     1   7   8  -5
+			index for second row	1     6   5   4  7
+			index for third row	2     2   15  6  12
 			index for fourth row	3   -100 12 700  6
-								   (i)												*/
+					       (i)												*/
 
 	//we want to find the sum of the elements above the main diagonal of the matrix
 	int sum = 0;
