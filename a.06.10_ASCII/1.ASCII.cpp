3ff60e422ed1c7bbf52294561f0e0d350d7869e2
todorjelev36@gmail.com <todorjelev36@gmail.com>
2014-12-16 11:22:46 +0200

    + added code for the exercise at 06.10

---
+++ a.06.10_ASCII/1.ASCII.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char symb = 'a';
+
+	//gets the ascii code of a, which is 97
+	int asciiCode = (int) symb;
+
+	cout << "the ASCII code of "<< symb << " is " << asciiCode << endl;
+
+	char symb2 = '5';
+
+	//the ascii code of 5 is 53
+	cout << "the ASCII code of "<< symb2 << " is " << (int)symb2 << endl;
+
+	cout << endl;
+
+	//on the other hand you can get symbol from the ascii code
+	
+	int code = 98;
+	char symbol3 = (char) code; //symbol3 must be b
+
+	cout << "the symbol with ASCII code " << code << " is " << symbol3 << endl;
+
+	system("pause");
+}
