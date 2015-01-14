9d1420291a596542474603332f78b412ad9209f4
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-01-14 23:55:52 +0200

    - change

--- n.14.01_Konsultacia/m.References.cpp
+++ n.14.01_Konsultacia/m.References.cpp
@@ -7,11 +7,11 @@ int main()
 	int var = 5;
 	int& ref = var; //zakachame ref kum var
 
-	ref++; //chrez prepratkata promeniame stoinostta na var na 6
+	ref++; //chrez psevdonima promeniame stoinostta na var na 6
 	cout << var << endl;
 
 	int var1 = 100;
-	ref = var1; //taka ne zakachame ref kum var1, a vzimame stoinostta na var1 i ia prisvoiavame na var chrez prepratkata
+	ref = var1; //taka ne zakachame ref kum var1, a vzimame stoinostta na var1 i ia prisvoiavame na var chrez psevdonima
 
 	//trite izkarvat edna i sushta stoinost
 	cout << var << " " << ref << " " << var1 << endl;
