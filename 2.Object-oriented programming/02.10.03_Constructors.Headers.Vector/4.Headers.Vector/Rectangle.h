a72fcf507f343218fde6ae0fff4215b35c02642d
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-03-10 23:57:18 +0200

    - clarifications

--- 2.Object-oriented programming/02.10.03_Constructors.Headers.Vector/4.Headers.Vector/Rectangle.h
+++ 2.Object-oriented programming/02.10.03_Constructors.Headers.Vector/4.Headers.Vector/Rectangle.h
@@ -1,4 +1,7 @@
-#pragma once
+//this is needed so that the header content is included only once.
+//#pragma once is not supported by all compilers. If it is not supported
+//use #ifndef..
+#pragma once 
 //#ifndef RECTANGLE_H
 //#define RECTANGLE_H
 enum Colors { Red, Green, Blue, Yellow };
