bed1e4c9c35d0422c46ceef5303f73cfeb52d67b
todorjelev36@gmail.com <todorjelev36@gmail.com>
2015-05-22 19:19:02 +0300

    - removing not needed lines

--- 2.OOP/10.21.05_list.pair.map/02.listImplementation.cpp
+++ 2.OOP/10.21.05_list.pair.map/02.listImplementation.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
 
 using namespace std;
 
--- 2.OOP/10.21.05_list.pair.map/06.mapTask.cpp
+++ 2.OOP/10.21.05_list.pair.map/06.mapTask.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 #include<map>
-#include<string>
-#include<vector>
 
 using namespace std;
 
